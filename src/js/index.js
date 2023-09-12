2bad74ac4c4ad1694b8e2af4e75b61e7407b13ef
ikafly144 <support@sabafly.net>
2023-09-12 14:44:24 +0900

    Revert "build失敗するのでコメントアウト"
    This reverts commit 1a10aac87f3ac567fa46abd8154c8722e21a3b3e.

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+const url = new URL(location.href);const params = new URLSearchParams(url.search);scrollTo(0, Number(params.get("scrollto")));console.log("[QueryScroller] Scrolled to", params.get("scrollto"));
