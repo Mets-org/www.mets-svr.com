3c0b6d4c33cca4f96f615f43993877cd376e2a88
ikafly144 <support@sabafly.net>
2023-09-12 15:29:31 +0900

    add query-squroller.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,10 @@ module.exports = {
     ],
     themes: ['@docusaurus/theme-classic'],
 
+    clientModules: [
+        require.resolve('./query-scroller.js'),
+    ],
+
     baseUrl: "/",
 
     title: "Met's Server",
