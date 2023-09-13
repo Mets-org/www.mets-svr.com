445883ee4f8ff70784c9f51c7ea0dff785893a0f
ikafly144 <support@sabafly.net>
2023-09-13 19:53:53 +0900

    崩れた表を修正 (#34)
    * 崩れた表を修正
    
    * mdxにしたり、localeをjaにしたり

--- src/theme/NotFound.js
+++ src/theme/NotFound.js
@@ -9,10 +9,7 @@ export default function NotFound() {
   return (
     <>
       <PageMetadata
-        title={translate({
-          id: 'theme.NotFound.title',
-          message: 'ページが見つかりません',
-        })}
+        title='ページが見つかりません'
       />
       <Layout>
         <main className="container margin-vert--xl">
@@ -22,28 +19,16 @@ export default function NotFound() {
           <div className="row">
             <div className="col col--6 col--offset-3">
               <h1 className="hero__title">
-                <Translate
-                  id="theme.NotFound.title"
-                  description="The title of the 404 page">
-                  道に迷われましたかな？
-                </Translate>
+                道に迷われましたかな？
               </h1>
               <MobileView>
                 <img src="https://media.tenor.com/ampW8g8v-vMAAAAd/hack-khaby.gif" style={{ padding: "1em" }} />
               </MobileView>
               <p>
-                <Translate
-                  id="theme.NotFound.p1"
-                  description="The first paragraph of the 404 page">
-                  ちょっとあなた！正しいURLを入力してもらえませんか？ただ正しいURLを入力する、簡単な事でしょう？
-                </Translate>
+                ちょっとあなた！正しいURLを入力してもらえませんか？ただ正しいURLを入力する、簡単な事でしょう？
               </p>
               <p>
-                <Translate
-                  id="theme.NotFound.p2"
-                  description="The 2nd paragraph of the 404 page">
-                  あなたがどうしても正しいURLにたどりつけないなら下の内容を詳しい人に伝えるといいかもしれませんね。
-                </Translate>
+                あなたがどうしても正しいURLにたどりつけないなら下の内容を詳しい人に伝えるといいかもしれませんね。
                 <BrowserOnly>
                   {
                     () => <CodeBlock language='yml'>
