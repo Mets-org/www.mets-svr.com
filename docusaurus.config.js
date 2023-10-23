2a7d062ac26666cf26e28ecf68230be225c439f6
ikafly144 <support@sabafly.net>
2023-10-23 21:07:10 +0900

    typescriptを使う

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+// @ts-check
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
     plugins: [
         [
             "@docusaurus/plugin-content-pages",
-            {
+            ({
                 path: "src/pages",
                 routeBasePath: "/",
                 include: ["**/*.{js,jsx,ts,tsx,md,mdx}"],
@@ -13,11 +16,11 @@ module.exports = {
                     "**/__tests__/**",
                 ],
                 mdxPageComponent: "@theme/MDXPage",
-            },
+            }),
         ],
         [
             "@docusaurus/plugin-content-blog",
-            {
+            ({
                 path: "blog",
                 blogTitle: "ブログ",
                 blogDescription: "誰も書かない(泣)",
@@ -42,26 +45,33 @@ module.exports = {
                 beforeDefaultRehypePlugins: [],
                 truncateMarker: /<!--\s*(truncate|見出し)\s*-->/,
                 showReadingTime: true,
-                feedOptions: {
-                    type: "all",
-                    title: "Met's Server Blog feed",
-                    description: "",
-                    copyright: `Copyright © ${new Date().getFullYear()} Met's Server`,
-                    language: undefined,
-                    createFeedItems: async (params) => {
-                        const { blogPosts, defaultCreateFeedItems, ...rest } = params;
-                        return defaultCreateFeedItems({
-                            // keep only the 10 most recent blog posts in the feed
-                            blogPosts: blogPosts.filter((item, index) => index < 10),
-                            ...rest,
-                        });
-                    },
-                },
-            },
+                feedOptions:
+                    /** @type {import('@docusaurus/plugin-content-blog').FeedOptions} */
+                    ({
+                        type: [
+                            'rss',
+                            'atom',
+                            'json',
+                        ],
+                        title: "Met's Server Blog feed",
+                        description: "",
+                        copyright: `Copyright © ${new Date().getFullYear()} Met's Server`,
+                        language: undefined,
+                        createFeedItems: async (params) => {
+                            const { blogPosts, defaultCreateFeedItems, ...rest } = params;
+                            return defaultCreateFeedItems({
+                                // keep only the 10 most recent blog posts in the feed
+                                blogPosts: blogPosts.filter((item, index) => index < 10),
+                                ...rest,
+                            });
+                        },
+                    }),
+            }),
         ],
         [
             "@docusaurus/plugin-client-redirects",
-            {
+            /** @type {import('@docusaurus/plugin-client-redirects').PluginOptions} */
+            ({
                 fromExtensions: ["html", "htm", "md", "mdx"], // /myPage.html -> /myPage
                 toExtensions: ["exe", "zip"], // /myAsset -> /myAsset.zip (存在すれば)
                 redirects: [
@@ -86,11 +96,11 @@ module.exports = {
                         from: ["/terms"],
                     },
                 ],
-            },
+            }),
         ],
         [
             "@docusaurus/plugin-content-docs",
-            {
+            ({
                 path: "docs",
                 breadcrumbs: true,
                 editUrl: ({ versionDocsDirPath, docPath }) =>
@@ -110,14 +120,22 @@ module.exports = {
                 remarkPlugins: [import("remark-math")],
                 showLastUpdateAuthor: true,
                 showLastUpdateTime: true,
-            },
+            }),
         ],
     ],
-    themes: ["@docusaurus/theme-classic"],
+    themes: [
+        [
+            "@docusaurus/theme-classic",
+            /** @type {import('@docusaurus/theme-classic').Options} */
+            ({
+                customCss: undefined,
+            })
+        ],
+    ],
 
     scripts: [
         {
-            src: '/js/query-scroller.js',
+            src: "/js/query-scroller.js",
             defer: true,
         },
     ],
@@ -143,88 +161,92 @@ module.exports = {
     projectName: "www.mets-svr.com",
     deploymentBranch: "gh-pages",
     favicon: "img/logo.svg",
-    themeConfig: {
-        colorMode: {
-            defaultMode: "light",
-            disableSwitch: false,
-            respectPrefersColorScheme: true,
-        },
-        navbar: {
-            title: "Met's Server",
-            logo: {
-                alt: "Logo",
-                src: "/img/logo.svg",
-                width: 32,
-                height: 32,
+    themeConfig:
+        /** @type {import('@docusaurus/types').ThemeConfig} */
+        ({
+            colorMode: {
+                defaultMode: "light",
+                disableSwitch: false,
+                respectPrefersColorScheme: true,
             },
-            items: [
-                {
-                    to: "blog",
-                    label: "Blog",
-                    position: "left",
+            navbar: {
+                title: "Met's Server",
+                logo: {
+                    alt: "Logo",
+                    src: "/img/logo.svg",
+                    width: 32,
+                    height: 32,
                 },
-                {
-                    to: "legal",
-                    label: "Terms",
-                    position: "left",
-                },
-            ],
-        },
-        footer: {
-            style: "dark",
-            links: [
-                {
-                    title: "Community",
-                    items: [
-                        {
-                            label: "Discord",
-                            href: "https://discord.gg/jG332tPs2D",
-                        },
-                    ],
-                },
-                {
-                    title: "Social",
-                    items: [
-                        {
-                            label: "GitHub",
-                            href: "https://github.com/Mets-org",
-                        },
-                        {
-                            label: "Twitter",
-                            href: "https://twitter.com/mets_server",
-                        },
-                    ],
-                },
-                {
-                    title: "Legal",
-                    items: [
-                        {
-                            label: "Terms",
-                            to: "legal/terms",
-                        },
-                        {
-                            label: "Minecraft Terms",
-                            to: "legal/minecraft",
-                        },
-                        {
-                            label: "Community Guideline",
-                            to: "legal/community-guideline",
-                        },
-                        {
-                            label: "Terms for Groups",
-                            to: "legal/group",
-                        },
-                    ],
-                },
-            ],
-            logo: {
-                alt: "Met's Server Logo",
-                src: "img/logo.svg",
-                href: "https://mets-svr.com/",
-                width: 32,
-                height: 32,
+                items: [
+                    {
+                        to: "blog",
+                        label: "Blog",
+                        position: "left",
+                    },
+                    {
+                        to: "legal",
+                        label: "Terms",
+                        position: "left",
+                    },
+                ],
             },
-            copyright: `Copyright © ${new Date().getFullYear()} Met's Server`,
-        },
-    },
+            footer: {
+                style: "dark",
+                links: [
+                    {
+                        title: "Community",
+                        items: [
+                            {
+                                label: "Discord",
+                                href: "https://discord.gg/jG332tPs2D",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Social",
+                        items: [
+                            {
+                                label: "GitHub",
+                                href: "https://github.com/Mets-org",
+                            },
+                            {
+                                label: "Twitter",
+                                href: "https://twitter.com/mets_server",
+                            },
+                        ],
+                    },
+                    {
+                        title: "Legal",
+                        items: [
+                            {
+                                label: "Terms",
+                                to: "legal/terms",
+                            },
+                            {
+                                label: "Minecraft Terms",
+                                to: "legal/minecraft",
+                            },
+                            {
+                                label: "Community Guideline",
+                                to: "legal/community-guideline",
+                            },
+                            {
+                                label: "Terms for Groups",
+                                to: "legal/group",
+                            },
+                        ],
+                    },
+                ],
+                logo: {
+                    alt: "Met's Server Logo",
+                    src: "img/logo.svg",
+                    href: "https://mets-svr.com/",
+                    width: 32,
+                    height: 32,
+                },
+                copyright: `Copyright © ${new Date().getFullYear()} Met's Server`,
+            },
+        }),
 };
+
+module.exports = config;
